0251182744597f142d2b8673ce337756ff40a7ed
EricksonLopes03 <ericklsantos3@gmail.com>
2021-04-09 01:02:00 -0300

    Implementando Tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -12,10 +12,30 @@
             if($_SESSION['id'] == '' && $_SESSION['nome'] == ''){ //protegendo página pessoal da aplicação
                 header('Location: /?login=erro');
             }else{
+                $tweet = Container::getModel('Tweet');
+                $tweet->__set('id_usuario', $_SESSION['id']);
+                $tweets = $tweet->getAll();
+                $this->view->tweets = $tweets;
                 $this->render('timeline');
             }
 
             
+        }
+
+        public function tweet(){
+            session_start();
+            if($_SESSION['id'] == '' && $_SESSION['nome'] == ''){ //protegendo página pessoal da aplicação
+                header('Location: /?login=erro');
+            }
+
+           $tweet = Container::getModel('Tweet');
+           $tweet->__set('tweet', $_POST['tweet']);
+           $tweet->__set('id_usuario', $_SESSION['id']);
+           $tweet->salvar();
+           header('Location: /timeline');
+
+
+
         }
     }
 ?>
---
+++ App/Models/Tweet.php
@@ -0,0 +1,46 @@
+<?php 
+    namespace App\ModelS;
+    use MF\Model\Model;
+
+    class Tweet extends Model{
+        private $id;
+        private $id_usuario;
+        private $tweet;
+        private $data;
+
+        public function __get($name)
+        {
+            return $this->$name;
+        }
+
+        public function __set($name, $value)
+        {
+            $this->$name = $value;
+        }
+
+        public function salvar(){
+            $query = 'insert into tweets (id_usuario, tweet) values (:id_usuario, :tweet)';
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+            $stmt->bindValue(':tweet', $this->__get('tweet'));
+            $stmt->execute();
+            return $this;
+
+
+        }
+
+        public function getAll(){
+            $query = "select tweets.id, tweets.id_usuario, tweets.tweet, DATE_FORMAT(tweets.data, '%d/%m/%Y %H:%i') data, usuarios.nome  from tweets
+                join usuarios on usuarios.id = tweets.id_usuario
+                where tweets.id_usuario = :id_usuario
+                ORDER by tweets.data desc";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+            $stmt->execute();
+
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        }
+          
+    }
+?>
