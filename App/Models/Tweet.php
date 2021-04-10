e69a193c7d53e98cffc9c6da9c862e41d5db8b62
EricksonLopes03 <ericklsantos3@gmail.com>
2021-04-10 03:00:08 -0300

    Exibindo tweets dos usuarios seguidos

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -32,7 +32,8 @@
         public function getAll(){
             $query = "select tweets.id, tweets.id_usuario, tweets.tweet, DATE_FORMAT(tweets.data, '%d/%m/%Y %H:%i') data, usuarios.nome  from tweets
                 join usuarios on usuarios.id = tweets.id_usuario
-                where tweets.id_usuario = :id_usuario
+                where tweets.id_usuario = :id_usuario or tweets.id_usuario in(select id_usuario_seguindo from usuarios_seguidores
+                where id_usuario = :id_usuario)
                 ORDER by tweets.data desc";
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
